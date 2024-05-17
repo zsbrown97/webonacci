9ccd135ac93688edec8bcd70b5226e211d2ba2d2
zsbrown97 <zackbrownbandofficial@gmail.com>
2024-05-17 13:54:39 -0500

    initial code and readme

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,23 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  let length: number = 5
+  let name: string = 'Zack';
+
+  function displaySequence(length: number) {
+    const result: Array<number> = [1, 1]
+
+    for (let i = 2; i < length; i++) {
+      result[i] = result[i - 1] + result[i - 2];
+    }
+    return result;
+  }
+</script>
+
+<div>
+  <h2>What is your name?</h2>
+  <input type="string" bind:value={name} />
+  <h1>Hello, {name}</h1>
+</div>
+<input type="number" bind:value={length} />
+
+<p>{length} numbers of the fibonacci sequence: </p>
+<h3>{displaySequence(length).join(', ')}</h3>
